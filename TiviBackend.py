063d62062eb7c3fd0e38ec8195dcbbef3d5d95a7
atum-martin <martin.atum.chapman@gmail.com>
2023-10-22 00:45:08 +0100

    rest api endpoints implemented. Basic epg loading implemented.

--- TiviBackend.py
+++ TiviBackend.py
@@ -1,8 +1,55 @@
 import sqlite3
 import json
+import http.server
+import socketserver
+from urllib.parse import urlparse, parse_qs
 
 con = sqlite3.connect("tivi.db")
 cur = con.cursor()
+
+class TiviBackendHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+    def tiviBackend(self):
+        print(self.path)
+        query_components = parse_qs(urlparse(self.path).query)
+
+        jsonReturn = []
+        if 'get_services' in query_components["action"]:
+            jsonReturn = getServices()
+        if 'get_epg' in query_components["action"]:
+            jsonReturn = getEpg((query_components["channel_id"]))
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps(jsonReturn), "utf8"))
+
+    def createM3U8(self):
+        print(self.path)
+        query_components = parse_qs(urlparse(self.path).query)
+
+        video_url = query_components["video"][0]
+        if video_url is None:
+            self.send_response(404)
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/x-mpegURL")
+        self.end_headers()
+        m3u8 = '#EXTM3U\n#EXTINF:-1 tvg-id="streamid" tvg-name="streamname" tvg-logo="" group-title="group1"\n'+str(video_url)
+        self.wfile.write(bytes(m3u8, "utf8"))
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'app3.html'
+        if '/backend.json' in self.path:
+            return self.tiviBackend()
+        elif '/stream?' in self.path:
+            return self.createM3U8()
+        else:
+            self.path = 'html/'+self.path
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+
 
 def countRows():
 
@@ -16,6 +63,12 @@ def getServices():
     res = cur.execute("SELECT live_service.id, live_service.name, logo, live_category.name, url, live_category.id FROM live_service INNER JOIN live_category ON live_service.group_id = live_category.id WHERE live_category.id = '1075' ORDER BY live_category.id ASC, live_service.id ASC")
     i = 100
     for row in res:
+
+        if row[0] == '':
+            continue
+
+        if i >= 120:
+            return services
         i += 1
         thisService = {
             "channel_id": row[0],
@@ -26,7 +79,6 @@ def getServices():
             "group_id": row[5]
         }
         services.append(thisService)
-    con.close()
     return services
 
 def getEpg(channelIds):
@@ -37,17 +89,15 @@ def getEpg(channelIds):
     i = 100
     for row in res:
         i += 1
+        #channelId, start, stop, title, desc
         thisService = {
-            "description": row[0],
-            "duration": row[1],#int seconds
-            "evtId": "",#int
-            "hasTstv": "false",
-            "image": "",
-            "name": i,
-            "startTime": "",#epoch in seconds
-            "svcId": '1'}
+            "channelId": row[0],
+            "start": row[1],
+            "stop": row[2],
+            "title": row[3],
+            "desc": row[4]
+        }
         services.append(thisService)
-    con.close()
     return services
 
 if __name__ == '__main__':
@@ -55,4 +105,9 @@ if __name__ == '__main__':
     countRows()
     print(str(json.dumps(getServices())))
     #print(str(getEpg(['de.Sky Cinema Hits'])))
+
+    handler_object = TiviBackendHttpRequestHandler
+    PORT = 8080
+    http_server = socketserver.TCPServer(("", PORT), handler_object)
+    http_server.serve_forever()
 
